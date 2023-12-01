ed58535092c74c7df6fee76ffb440da0fc907847
Davide Lodde <johndoe@example.com>
2023-12-01 14:43:38 +0100

    Davide

---
+++ script.js
@@ -0,0 +1,14 @@
+const team = [
+    {
+        name: "Davide",
+        surname: "Lodde",
+        age: 30,
+        city: "Cagliari, Sardegna",
+        hobby: "Development",
+        favoriteFood: "Impossibile scegliere",
+        favoriteVideoGame: "Impossibile scegliere",
+        favoriteFilm: "Impossibile scegliere",
+        favoriteBook: "Impossibile scegliere",
+        petName: "Impossibile scegliere"
+    }
+];
