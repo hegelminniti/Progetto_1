0a3000f09a4f923bedb904441976eed9db80f841
Alex <al.vaccaro@outlook.com>
2023-12-01 16:13:30 +0100

    Upload funzioni di Sorting di Alessandro su branch personale

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/*
+- Print the team in alphabetical order (surname name).
+- Print the team in age order (name age).
+- Print the team middle age.
+- Print who has a pet (name petName).
+- Print who wrote ‘LOL’ or ‘League Of Legends’ as a favorite video game. (name).
+- Print if there are some members with the same name (name).
+*/
+
 const team = [
     {
         name: "Hegel",
@@ -59,5 +68,37 @@ const team = [
         petName: "Impossibile scegliere"
     }
 ];
+
+function ageSort(array) { // Funzione sorting decrescente di età (ALESSANDRO)
+    let sortedAges = array.sort(
+        (p1, p2) => (p1.age < p2.age) ? 1 : (p1.age > p2.age) ? -1 : 0);
+        
+    return sortedAges;
+}
+
+function surnameSort(array) { // Funzione di sorting descrescente per cognome (ALESSANDRO)
+    array.sort(function(a,b){
+        const nameA = a.surname.toUpperCase(); // ignore upper and lowercase
+        const nameB = b.surname.toUpperCase(); // ignore upper and lowercase
+        if (nameA > nameB) {
+            return -1;
+        }
+        if (nameA < nameB) {
+            return 1;
+        }
+    });
+    return array;
+}
+
+let ageArray = ageSort(team.slice(0))
+let sortedName = surnameSort(team)
+
+for(let i=0; i < sortedName.length; i++){ // print Surname - Name in ordine Alfabetico per cognome
+    console.log(`Surname : ${sortedName[i].surname} , Name : ${sortedName[i].name}`);
+}
+
+for(let i=0; i < ageArray.length; i++ ){ // print Name - Age in ordine di età
+    console.log(`Name : ${ageArray[i].name}, Age : ${ageArray[i].age}`);
+}
 
 
