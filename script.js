500f504de50792ad200f1d56e8cab891416a9cf7
Giampiero <giampierof2011@hotmail.it>
2023-12-01 15:10:17 +0100

    ho fatto oggetto

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const team = [];
+const team = [ {
+    name: "Giampiero",
+    surname: "Franconieri",
+    age: 25,
+    city: "Rizziconi",
+    hobby: "Power Lifting",
+    favoriteFood: "Spaghetti allo scoglio",
+    favoriteVideoGame: "Forza Horizon 4",
+    favoriteFilm: "Il buono, il brutto e il cattivo",
+    favoriteBook: "",
+    petName: ""
+    }];
