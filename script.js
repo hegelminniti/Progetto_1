65f04add6de9c523dc29177bc30b2016ce133f0a
Alex <al.vaccaro@outlook.com>
2023-12-01 15:03:02 +0100

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const team = [];
+const team = [{
+    name: "Alessandro",
+    surname: "Vaccaro",
+    age: 31,
+    city: "Pisa",
+    hobby: "Wrestling, musica , videogames",
+    favoriteFood: "Pizza",
+    favoriteVideoGame: "World Of Warcraft",
+    favoriteFilm: "Il Miglio Verde",
+    favoriteBook: "Il Ciclo dell'Odio",
+    petName: "Giorgio"
+}];
