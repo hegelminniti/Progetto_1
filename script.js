7df34b7e58a9b8b3da50fb0e5a593e6539df6c4e
Hegsina <hegelminniti@gmail.com>
2023-12-01 16:47:28 +0100

    Implemented function that sorts favorite game

--- script.js
+++ script.js
@@ -1,63 +1,79 @@
 const team = [
-    {
-        name: "Hegel",
-        surname: "Minniti",
-        age: 23,
-        hobby: "Musica, videogames",
-        favoriteFood: "Impossibile scegliere",
-        favoriteVideoGame: "Palia",
-        favoriteFilm: "Sette anime",
-        favoriteBook: "Nessuno si salva da solo",
-        petName: "Ginger, Yuki, Reneé, Ciccio",
-    },
-    {
-        name: "Martina",
-        surname: "Mancuso",
-        age: 32,
-        city: "Milano",
-        hobby: "reading",
-        favoriteFood: "indian food",
-        favoriteVideoGame: "tomb rider",
-        favoriteFilm: "mamma mia",
-        favoriteBook: "solo bagaglio a mano",
-        petName: "Neve",
-    },
-    {
-        name: "Alessandro",
-        surname: "Vaccaro",
-        age: 31,
-        city: "Pisa",
-        hobby: "Wrestling, musica , videogames",
-        favoriteFood: "Pizza",
-        favoriteVideoGame: "World Of Warcraft",
-        favoriteFilm: "Il Miglio Verde",
-        favoriteBook: "Il Ciclo dell'Odio",
-        petName: "Giorgio",
-    },
-    {
-        name: "Giampiero",
-        surname: "Franconieri",
-        age: 25,
-        city: "Rizziconi",
-        hobby: "Power Lifting",
-        favoriteFood: "Spaghetti allo scoglio",
-        favoriteVideoGame: "Forza Horizon 4",
-        favoriteFilm: "Il buono, il brutto e il cattivo",
-        favoriteBook: "",
-        petName: ""
-    },
-    {
-        name: "Davide",
-        surname: "Lodde",
-        age: 30,
-        city: "Cagliari, Sardegna",
-        hobby: "Development",
-        favoriteFood: "Impossibile scegliere",
-        favoriteVideoGame: "Impossibile scegliere",
-        favoriteFilm: "Impossibile scegliere",
-        favoriteBook: "Impossibile scegliere",
-        petName: "Impossibile scegliere"
-    }
+  {
+    name: "Hegel",
+    surname: "Minniti",
+    age: 23,
+    hobby: "Musica, videogames",
+    favoriteFood: "Impossibile scegliere",
+    favoriteVideoGame: "Palia",
+    favoriteFilm: "Sette anime",
+    favoriteBook: "Nessuno si salva da solo",
+    petName: "Ginger, Yuki, Reneé, Ciccio",
+  },
+  {
+    name: "Martina",
+    surname: "Mancuso",
+    age: 32,
+    city: "Milano",
+    hobby: "reading",
+    favoriteFood: "indian food",
+    favoriteVideoGame: "tomb rider",
+    favoriteFilm: "mamma mia",
+    favoriteBook: "solo bagaglio a mano",
+    petName: "Neve",
+  },
+  {
+    name: "Alessandro",
+    surname: "Vaccaro",
+    age: 31,
+    city: "Pisa",
+    hobby: "Wrestling, musica , videogames",
+    favoriteFood: "Pizza",
+    favoriteVideoGame: "World Of Warcraft",
+    favoriteFilm: "Il Miglio Verde",
+    favoriteBook: "Il Ciclo dell'Odio",
+    petName: "Giorgio",
+  },
+  {
+    name: "Giampiero",
+    surname: "Franconieri",
+    age: 25,
+    city: "Rizziconi",
+    hobby: "Power Lifting",
+    favoriteFood: "Spaghetti allo scoglio",
+    favoriteVideoGame: "Forza Horizon 4",
+    favoriteFilm: "Il buono, il brutto e il cattivo",
+    favoriteBook: "",
+    petName: "",
+  },
+  {
+    name: "Davide",
+    surname: "Lodde",
+    age: 30,
+    city: "Cagliari, Sardegna",
+    hobby: "Development",
+    favoriteFood: "Impossibile scegliere",
+    favoriteVideoGame: "Impossibile scegliere",
+    favoriteFilm: "Impossibile scegliere",
+    favoriteBook: "Impossibile scegliere",
+    petName: "Impossibile scegliere",
+  },
 ];
 
+function favGame(array) {
+  const lolPlayers = array
+    .filter(
+      (member) =>
+        member.favoriteVideoGame.toLowerCase() === "lol" ||
+        member.favoriteVideoGame.toLowerCase() === "league of legends"
+    )
+    .map((member) => member.name);
 
+  if (lolPlayers.length === 0) {
+    console.log("Nessuno ha scelto come gioco preferito League of Legends.");
+  } else {
+    console.log(lolPlayers);
+  }
+}
+
+favGame(team);
