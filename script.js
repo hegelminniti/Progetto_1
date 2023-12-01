b215936b0db33ff48bb5c069e46adc76a1373cee
Hegsina <hegelminniti@gmail.com>
2023-12-01 12:57:07 +0100

    aggiunto oggetto al file js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const team = [];
+const team = [
+    {
+        name: "Hegel",
+        surname: "Minniti",
+        age: 23,
+        hobby: "Musica, videogames",
+        favoriteFood: "Impossibile scegliere",
+        favoriteVideoGame: "Palia",
+        favoriteFilm: "Sette anime",
+        favoriteBook: "Nessuno si salva da solo",
+        petName: "Ginger, Yuki, Reneé, Ciccio"
+    }
+];
