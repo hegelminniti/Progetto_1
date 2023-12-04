8bcc8490c8e3a1d5a68afbf1e801ef2f15fdd48e
Giampiero <giampierof2011@hotmail.it>
2023-12-04 11:56:31 +0100

    modifiche 1

--- script.js
+++ script.js
@@ -61,26 +61,3 @@ const team = [
 ];
 
 
-// Giampiero Franconieri
-// Print if there are some members with the same name (name)
-// Stampa se ci sono membri con lo stesso nome (nome)
-function sameName(array){
-    let nomi=[];//variabile contenti solo i nomi degli oggetti
-    array.forEach((user,i) => {//metodo per trasferire i nomi degli utenti
-        nomi[i]=user.name;
-    });
-    let x=0//indice di egualName
-    let egualName=[];//array contenenti i nomi duplicati
-    for(let i in nomi)
-        for(let j in nomi)// doppio for per iterare tutte le combinazioni nome:nome
-            if((nomi[i]==nomi[j])&& (i!=j)) //controllo se ci sono nomi duplicati evitando di 'controllare se stesso'
-            {
-                egualName[x]=nomi[i];//salvataggio nomi duplicati
-                x++;
-            }
-    egualName= [...new Set (egualName)];// elimino nomi duplicati per non stamparli più volte
-    if(egualName[0])   //verifico esistenza di nomi duplicati    
-        console.log(egualName);//stampo nomi duplicati
-    else console.log("Nessun utente con lo stesso nome.");//se non ci sono doppi nomi rifesrisco quanto scritto.
-}
-sameName(team);
