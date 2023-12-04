08c8dd3d806518c0c72ee6d23170322161f70d5a
Davide Lodde <johndoe@example.com>
2023-12-04 12:17:25 +0100

    aggiornamento Davide

--- script.js
+++ script.js
@@ -71,97 +71,101 @@ const team = [
 ];
 
 function ageSort(array) { // Funzione sorting decrescente di età (ALESSANDRO)
-  let sortedAges = array.sort(
-      (p1, p2) => (p1.age < p2.age) ? 1 : (p1.age > p2.age) ? -1 : 0);
-      
-  return sortedAges;
+    let sortedAges = array.sort(
+        (p1, p2) => (p1.age < p2.age) ? 1 : (p1.age > p2.age) ? -1 : 0);
+
+    return sortedAges;
 }
 
 function surnameSort(array) { // Funzione di sorting descrescente per cognome (ALESSANDRO)
-  array.sort(function(a,b){
-      const nameA = a.surname.toUpperCase(); // ignore upper and lowercase
-      const nameB = b.surname.toUpperCase(); // ignore upper and lowercase
-      if (nameA > nameB) {
-          return -1;
-      }
-      if (nameA < nameB) {
-          return 1;
-      }
-  });
-  return array;
+    array.sort(function (a, b) {
+        const nameA = a.surname.toUpperCase(); // ignore upper and lowercase
+        const nameB = b.surname.toUpperCase(); // ignore upper and lowercase
+        if (nameA > nameB) {
+            return -1;
+        }
+        if (nameA < nameB) {
+            return 1;
+        }
+    });
+    return array;
 }
 
 // Martina: 'Print the team middle age'
 function middleAge(array) {
-  let sumAge = array.reduce((a, num) => a + num.age, 0);
-  let avgAge = sumAge / array.length;
-  return avgAge;
+    let sumAge = array.reduce((a, num) => a + num.age, 0);
+    let avgAge = sumAge / array.length;
+    return avgAge;
 };
 
+// funzione Davide
+
+// Itera attraverso l'array e stampa chi ha un animale domestico e il nome dell'animale
+function pet(array) {
+    for (let persona of array) {
+        if (persona.petName && persona.petName !== "Impossibile scegliere") {
+            console.log(`${persona.name} ${persona.surname} ha un animale domestico di nome ${persona.petName}.`);
+        } else {
+            console.log(`${persona.name} ${persona.surname} non ha un animale domestico.`);
+        }
+    }
+}
 // funzione Hegel
 // Print who wrote ‘LOL’ or ‘League Of Legends’ as a favorite video game. (name).
 
 function favGame(array) {
-  const lolPlayers = array
-    .filter(
-      (member) =>
-        member.favoriteVideoGame.toLowerCase() === "lol" ||
-        member.favoriteVideoGame.toLowerCase() === "league of legends"
-    )
-    .map((member) => member.name);
+    const lolPlayers = array
+        .filter(
+            (member) =>
+                member.favoriteVideoGame.toLowerCase() === "lol" ||
+                member.favoriteVideoGame.toLowerCase() === "league of legends"
+        )
+        .map((member) => member.name);
 
-  if (lolPlayers.length === 0) {
-    console.log("Nessuno ha scelto come gioco preferito League of Legends.");
-  } else {
-    console.log(lolPlayers);
-  }
+    if (lolPlayers.length === 0) {
+        console.log("Nessuno ha scelto come gioco preferito League of Legends.");
+    } else {
+        console.log(lolPlayers);
+    }
 }
 // Giampiero Franconieri
 // Print if there are some members with the same name (name)
 // Stampa se ci sono membri con lo stesso nome (nome)
-function sameName(array){
-    let nomi=[];//variabile contenti solo i nomi degli oggetti
-    array.forEach((user,i) => {//metodo per trasferire i nomi degli utenti
-        nomi[i]=user.name;
+function sameName(array) {
+    let nomi = [];//variabile contenti solo i nomi degli oggetti
+    array.forEach((user, i) => {//metodo per trasferire i nomi degli utenti
+        nomi[i] = user.name;
     });
-    let x=0//indice di egualName
-    let egualName=[];//array contenenti i nomi duplicati
-    for(let i in nomi)
-        for(let j in nomi)// doppio for per iterare tutte le combinazioni nome:nome
-            if((nomi[i]==nomi[j])&& (i!=j)) //controllo se ci sono nomi duplicati evitando di 'controllare se stesso'
+    let x = 0//indice di egualName
+    let egualName = [];//array contenenti i nomi duplicati
+    for (let i in nomi)
+        for (let j in nomi)// doppio for per iterare tutte le combinazioni nome:nome
+            if ((nomi[i] == nomi[j]) && (i != j)) //controllo se ci sono nomi duplicati evitando di 'controllare se stesso'
             {
-                egualName[x]=nomi[i];//salvataggio nomi duplicati
+                egualName[x] = nomi[i];//salvataggio nomi duplicati
                 x++;
             }
-    egualName= [...new Set (egualName)];// elimino nomi duplicati per non stamparli più volte
-    if(egualName[0])   //verifico esistenza di nomi duplicati    
+    egualName = [...new Set(egualName)];// elimino nomi duplicati per non stamparli più volte
+    if (egualName[0])   //verifico esistenza di nomi duplicati    
         console.log(egualName);//stampo nomi duplicati
     else console.log("Nessun utente con lo stesso nome.");//se non ci sono doppi nomi rifesrisco quanto scritto.
 }
 let ageArray = ageSort(team.slice(0))
 let sortedName = surnameSort(team)
 /*------MAIN------*/
-for(let i=0; i < sortedName.length; i++){ // print Surname - Name in ordine Alfabetico per cognome
-  console.log(`Surname : ${sortedName[i].surname} , Name : ${sortedName[i].name}`);
+for (let i = 0; i < sortedName.length; i++) { // print Surname - Name in ordine Alfabetico per cognome
+    console.log(`Surname : ${sortedName[i].surname} , Name : ${sortedName[i].name}`);
 }
 
-for(let i=0; i < ageArray.length; i++ ){ // print Name - Age in ordine di età
-  console.log(`Name : ${ageArray[i].name}, Age : ${ageArray[i].age}`);
+for (let i = 0; i < ageArray.length; i++) { // print Name - Age in ordine di età
+    console.log(`Name : ${ageArray[i].name}, Age : ${ageArray[i].age}`);
 }
 
 console.log(middleAge(team));
+pet(team);
 
-// funzione Davide
-
-// Itera attraverso l'array e stampa chi ha un animale domestico e il nome dell'animale
-for (let persona of team) {
-    if (persona.petName && persona.petName !== "Impossibile scegliere") {
-        console.log(`${persona.name} ${persona.surname} ha un animale domestico di nome ${persona.petName}.`);
-    } else {
-        console.log(`${persona.name} ${persona.surname} non ha un animale domestico.`);
-    }
-  }
 
 favGame(team);
 
 sameName(team);
+
